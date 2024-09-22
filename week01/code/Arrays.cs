fb06042f79565c6bbfadf824dc0bbba658d6acf3
benjamincouto <ben.couto@live.com>
2024-09-21 22:30:27 -0300

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,17 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Define list to store multiples of "number"
+        List<double> results = new();
+
+        //FOR loop to multiply "number" with number from 1 to "length" to get multiples, add them to results
+        for (double i = 1; i < length; i++)
+        {
+            results.Add(number * i);
+        }
+
+        //convert list to array, return results
+        return results.ToArray(); // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +39,17 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        
+        //Slice list from index 0 to "amount"
+        List<int> firstPart = data.GetRange(0, data.Count - amount);
+        //Slice list from "amount" to count
+        List<int> lastPart = data.GetRange(data.Count - amount, amount);
+
+        //Combine both to define new list by clearing the original list and adding both parts to data
+        data.Clear();
+        data.AddRange(lastPart);
+        data.AddRange(firstPart);
+        
     }
 }
