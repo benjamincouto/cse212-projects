1a6ff4a274da7ae47cd057a9fc9d1266dd1abad4
benjamincouto <ben.couto@live.com>
2024-10-28 22:35:03 -0300

    w06 code

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,10 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+        {
+            return; // Value already exists in the tree; do nothing
+        }
 
         if (value < Data)
         {
@@ -34,12 +38,47 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (value == Data)
+        {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            // Check left
+            if (Left is not null && Left.Contains(value))
+                {
+                    return true;
+                }
+        }
+        else
+        {
+            // Check right
+            if (Right is not null && Right.Contains(value))
+                {
+                    return true;
+                }
+        }
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+
+        // The scenario for an empty node is already handled in the BinarySearchTree class
+        // Return 1 if no leaves
+        if (Left == null && Right == null)
+        {
+            return 1;
+        }
+
+        // if left or right, recursively call GetHeigh, otherwise -1
+        int leftHeight = Left?.GetHeight() ?? -1;
+        int rightHeight = Right?.GetHeight() ?? -1;
+
+        
+        return Math.Max(leftHeight, rightHeight) + 1;
+
     }
 }
